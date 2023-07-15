8ffed3bde360dcbdd25d77780d3eff7c99937666
alexander <aleksandrpereselkov77@gmail.com>
2023-07-15 18:49:04 +0200

    mailer

--- config/console.php
+++ config/console.php
@@ -44,6 +44,29 @@ $config = [
 
         ],
     ],
+    'mailer' => [
+        'class' => 'yii\swiftmailer\Mailer',
+        // send all mails to a file by default. You have to set
+        // 'useFileTransport' to false and configure a transport
+        // for the mailer to send real emails.
+        'transport' => [
+            'class' => 'Swift_SmtpTransport',
+            'host' => 'mail.up-drop.ru',
+            'username' => getenv('APP_MAILER_USERNAME'),
+            'password' => getenv('APP_MAILER_PASSWORD'),
+            'port' => 465,
+            'encryption' => 'ssl',
+            'streamOptions' => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ],
+            ],
+        ],
+        'enableSwiftMailerLogging' =>false,
+        'useFileTransport' => false,
+    ],
     'modules' => [
         'user' => [
             'class' => 'dektrium\user\Module',
