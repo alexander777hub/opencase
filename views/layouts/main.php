5418a3a898213d7b6ed39c2e50b59cadb5caacca
alexander <aleksandrpereselkov77@gmail.com>
2023-07-23 13:32:20 +0200

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,12 +4,9 @@
 /** @var string $content */
 
 use app\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
+
 use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
-use app\models\Profile;
-use app\models\Userform;
+
 use app\models\User;
 
 
@@ -23,7 +20,7 @@ if(!Yii::$app->user->isGuest){
     $name = User::getUser(Yii::$app->user->getId())->getProfile() ? User::getUser(Yii::$app->user->getId())->getProfile()->getName() : User::getUser(Yii::$app->user->getId())->username;
 
 
-    if(!User::getUser(Yii::$app->user->getId())->getProfile()->getName() && !User::getUser(Yii::$app->user->getId())->username){
+    if(User::getUser(Yii::$app->user->getId())->getProfile() && !User::getUser(Yii::$app->user->getId())->getProfile()->getName() && !User::getUser(Yii::$app->user->getId())->username){
         $name = 'Не задано';
     }
 }
