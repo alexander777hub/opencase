023fe8b9eef1011f4fe21a11e6a4c1cd0f0b62aa
alexander <aleksandrpereselkov77@gmail.com>
2023-07-27 04:31:25 +0200

    icon

--- modules/mng/views/item/_form.php
+++ modules/mng/views/item/_form.php
@@ -10,42 +10,9 @@ use yii\widgets\ActiveForm;
 
 ?>
 
-<script type="text/javascript">
-    $(document).ready(function(){
-
-        //  $( ".inner" ).append( "<p>Test</p>" );
-        var divs = $(".card-body");
-        console.log(divs, "DIVS");
-        var avatar = "<?= $model && $model->icon ? $model->icon : "/uploads/profile/default.png"  ?>";
-        $("#icon").attr("src", avatar);
-        $(".card-body").each(function (i) {
-            var id = $(this).next().find('img').attr('id');
-            var rem = $(this).find('.rem');
-            rem.remove();
-            var userform_id =  "<?= strval($model->id) ?>";
-            console.log(userform_id,"UFID");
 
 
-            var href = '/mng/item/remove-photo?photo_id=' + id;
-            $(this).append("<a href=" + href +
-                " class='btn btn-danger ml-2'>Удалить</a>" );
-
-
-        });
-        if($('.avatar')){
-            $('.avatar').on("click", function(e){
-                // e.preventDefault();
-                var id =  $(this).attr("data-id");
-
-
-            });
-
-        }
-    })
-
-</script>
-
-< class="item-form">
+<div class="item-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
