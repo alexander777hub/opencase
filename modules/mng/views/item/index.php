bc1343231a71879cf506ac0ab338c1ad9ae6ed1d
alexander <aleksandrpereselkov77@gmail.com>
2023-08-01 09:57:47 +0200

    fix

--- modules/mng/views/item/index.php
+++ modules/mng/views/item/index.php
@@ -30,7 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'appid',
+            'price',
             'classid',
             'instanceid',
             'market_hash_name',
