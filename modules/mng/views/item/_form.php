037a3ef3daf74627cdea7843833c46f0ccb24bd0
alexander <aleksandrpereselkov77@gmail.com>
2023-08-02 15:23:28 +0200

    fix

--- modules/mng/views/item/_form.php
+++ modules/mng/views/item/_form.php
@@ -19,7 +19,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'rarity')->textInput() ?>
     <?= $form->field($model, 'exterior')->textInput() ?>
     <?= $form->field($model, 'icon_url')->textInput(['maxlength' => true]) ?>
-    <?= $form->field($model, 'internal_name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'market_hash_name')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
     <img src=<?php $model->icon_url ? 'https://community.cloudflare.steamstatic.com/economy/image/' . $model->icon_url . '/image.png ' : '/uploads/profile/default.png' ?>>
     <div class="form-group">
