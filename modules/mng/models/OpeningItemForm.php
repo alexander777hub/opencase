62bf9fd5ac7bf9bfa3049d80c0be3ce4b38f39da
alexander <aleksandrpereselkov77@gmail.com>
2023-08-07 14:59:31 +0200

    fix case

--- modules/mng/models/OpeningItemForm.php
+++ modules/mng/models/OpeningItemForm.php
@@ -21,22 +21,51 @@ class OpeningItemForm extends BaseObject
     public static function getCase()
     {
         $cases = [];
-        foreach(Opening::getRarityList() as $val){
-            foreach (Opening::getExteriorList() as $ext){
-                $query = (new \yii\db\Query())->select(['*'])->from('item')->where(['rarity' => $val['name'], 'exterior'=> $ext['name']]);
+        $cases_random = [];
+        $sql = [];
+        foreach (Opening::getRarityList() as $k => $val) {
+            foreach (Opening::getExteriorList() as $ext) {
+                $query = (new \yii\db\Query())->select(['*'])->from('item')->where(['rarity' => $val['name'], 'exterior' => $ext['name']]);
                 $raw = $query->createCommand()->getRawSql();
+
                 $command = $query->createCommand();
                 $data = $command->queryAll();
-                if(!empty($data)){
-                    foreach($data as $item){
-                        $cases[] = $item['id'];
+                if (!empty($data)) {
+                    foreach ($data as $item) {
+                        $cases[$val['name']][$ext['name']][] = $item['id'];
                     }
-                  //  $cases[] = $data[mt_rand(0, count($data)-1)]['id'];
+
+
                 }
             }
-
         }
-        return $cases;
+        $cases_random = [];
+        foreach($cases as $k => $case){
+            $c = $case;
+            foreach ($case as $r => &$item){
+                shuffle($item);
+                foreach ($item as $key => $value){
+                    if($key == 10){
+                        break;
+                    }
+                    $cases_random[$k][$r][] = $value;
+                }
+            }
+        }
+
+
+        $final_case = [];
+        foreach($cases_random as $rarity => $exteriors){
+            foreach($exteriors as $ext){
+                foreach($ext as $id) {
+                    $final_case[] = $id;
+                }
+            }
+        }
+
+
+        $f = $final_case;
+        return $final_case;
 
     }
 
@@ -44,7 +73,7 @@ class OpeningItemForm extends BaseObject
     public function create()
     {
         $cases = self::getCase();
-        $ids = array_merge($cases, $this->model->item_ids);
+        $ids = array_unique(array_merge($cases, $this->model->item_ids));
         foreach($ids as $item){
             $model = new OpeningItemInit();
 
--- views/site/_caseItem.php
+++ views/site/_caseItem.php
@@ -8,6 +8,7 @@
         <img src=<?= $model->icon_url   ?> srcset="https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot6-iFA957PLddgJW4864q4-ZlvnzNoTdn2xZ_Isk2euUpI2l2gaw-Es5Mmr6cdDDd1BoMw2E81O7xO-71pG4vcjIziFgpGB8siZU0y43/260fx194f/image.png 2x" class="skin__img">
         <div class="skin__info">
             <div class="skin__type"><?= $model->type    ?></div>
+            <div class="skin__type"><?= $model->rarity    ?></div>
             <div class="skin__name"><?= $model->market_hash_name    ?></div>
         </div>
     </div>
