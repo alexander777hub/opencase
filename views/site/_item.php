1a635512cf8cf99f37ea3d8e68a1094eb9a2d45a
alexander <aleksandrpereselkov77@gmail.com>
2023-08-10 06:18:53 +0200

    fix price

--- views/site/_item.php
+++ views/site/_item.php
@@ -11,13 +11,13 @@
 
     <a href=<?= "/site/view?id=" . $model->id ?>  class="case item-limited">
 
-        <div class="case__limit">
+       <!-- <div class="case__limit">
             <div class="case__limit-inner">
                 <div class="case__limit-left">31780</div>
                 <div class="case__limit-separator">/</div>
                 <div class="case__limit-reserve">35000</div>
             </div>
-        </div>
+        </div>  !-->
 
         <div class="case__wrapper-img">
             <img src=<?= $model->avatar_id ? \app\modules\mng\models\Opening::getOriginal($model->avatar_id)  : "https://images.steamcdn.io/forcedrop/cases/summerrrrr.png"  ?> class="case__img">
@@ -31,7 +31,7 @@
 
         <div class="case__price">
             <div class="case__current-price">
-                <span class="price price-RUB"><?= $model->price  ?></span>
+                <span class="price price-RUB"><?= round($model->price, 0)  ?></span>
             </div>
         </div>
 
