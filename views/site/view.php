726094e050b43a389939d73e4cc03324e2148ab9
alexander <aleksandrpereselkov77@gmail.com>
2023-09-04 08:20:07 +0200

    ашч

--- views/site/view.php
+++ views/site/view.php
@@ -68,21 +68,6 @@ if(!Yii::$app->user->isGuest){
             parent.addClass(jsClass);
         });
 
-        let itemsRoll = {
-            simple: {
-                skin: "M4A1-S | Cyrex",
-                img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_m4a1_silencer_cu_m4a1s_cyrex_light_large.144b4053eb73b4a47f8128ebb0e808d8e28f5b9c.png"
-            },
-            middle: {
-                skin: "M4A1-S | Chantico's Fire",
-                img: "https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpou-6kejhz2v_Nfz5H_uO1gb-Gw_alIITCmX5d_MR6j_v--YXygED6_UZrMTzwJYSdJlU8N1zY81TrxO_v0MW9uJnBm3Rk7nEk5XfUmEeyhQYMMLIUhCYx0A"
-            },
-            super: {
-                skin: "M4A4 | Asiimov",
-                img: "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_m4a1_cu_m4_asimov_light_large.af03179f3d43ff55b0c3d114c537eac77abdb6cf.png"
-            }
-        };
-
 
             $("#open").on("click", function(){
                 if($(this).hasClass('noclick')){
@@ -425,8 +410,6 @@ if(!Yii::$app->user->isGuest){
                 <div class="case-page-fav__icon btn__icon icon icon_fav"></div>
                 <div class="case-page-fav__label btn__label">
 
-                    Добавить в избранное
-
                 </div>
             </div>
         </div>
@@ -437,7 +420,7 @@ if(!Yii::$app->user->isGuest){
             </div>
         </div>
     </div>
-    <center><span id="win" style="font-size: 25px;"> <span style="
+    <center><span  id="win" style="font-size: 25px;"> <span style="
     color: green;" id="rolled"></span>
 <br>
 
